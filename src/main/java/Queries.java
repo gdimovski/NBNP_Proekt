adcf97b8b05a5f34d88220b213b2ade71d257a11
Gorazd Dimovski <gorazd.students.finki.ukim.mk>
2023-02-09 01:43:46 +0100

    Add more complex queries

--- src/main/java/Queries.java
+++ src/main/java/Queries.java
@@ -4,12 +4,14 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.filter.CompareFilter;
-import org.apache.hadoop.hbase.filter.FilterList;
-import org.apache.hadoop.hbase.filter.SingleColumnValueExcludeFilter;
-import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
+import org.apache.hadoop.hbase.filter.*;
 import org.apache.hadoop.hbase.util.Bytes;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -219,7 +221,156 @@ public class Queries {
         map.put("Time taken to execute the query 'Get all rows with values in a specified range in a single column and return only specific columns' is: " + duration + " milliseconds", Double.valueOf(count));
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void getPM_MeasurementsInDecember(Table table, HashMap<String,Double> map) throws IOException {
+        long startTime = System.currentTimeMillis();
+        Scan scan = new Scan();
+        SingleColumnValueFilter filter = new SingleColumnValueFilter(
+                Bytes.toBytes("data"),
+                Bytes.toBytes("Measurement date"),
+                CompareFilter.CompareOp.EQUAL,
+                new RegexStringComparator("^*-12-*$")
+        );
+        scan.setFilter(filter);
+
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("PM2.5"));
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("PM10"));
+
+        ResultScanner resultScanner = table.getScanner(scan);
+        for (Result result : resultScanner) {
+            byte[] pm25 = result.getValue(Bytes.toBytes("data"), Bytes.toBytes("PM2.5"));
+            byte[] pm10 = result.getValue(Bytes.toBytes("data"), Bytes.toBytes("PM10"));
+
+            System.out.println("Measurement date: " + Bytes.toString(result.getRow()));
+            System.out.println("PM2.5: " + Float.valueOf(Bytes.toString(pm25)));
+            System.out.println("PM10: " + Float.valueOf(Bytes.toString(pm10)));
+        }
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        map.put("Time taken to execute the query 'PM measurements in december' is: " + duration + " milliseconds", 1d);
+    }
+
+    public static void getTimeAndStationWithHighestAverageValueOfPollution(Table table, HashMap<String, Double> map) throws IOException {
+        long startTime = System.currentTimeMillis();
+        Scan scan = new Scan();
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("Station code"));
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("SO2"));
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("NO2"));
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("CO"));
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("O3"));
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("PM10"));
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("PM2.5"));
+
+        ResultScanner resultScanner = table.getScanner(scan);
+
+        int stationCode = 0;
+        String date = "";
+        float maxAvg = 0;
+
+        for (Result result : resultScanner) {
+            String currentDate = Bytes.toString(result.getRow());
+            int currentStationCode = Integer.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("Station code"))));
+            float so2 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("SO2"))));
+            float no2 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("NO2"))));
+            float co = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("CO"))));
+            float o3 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("O3"))));
+            float pm10 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("PM10"))));
+            float pm25 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("PM2.5"))));
+
+            float avg = (so2 + no2 + co + o3 + pm10 + pm25) / 6;
+            if (avg > maxAvg) {
+                maxAvg = avg;
+                stationCode = currentStationCode;
+                date = currentDate;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        map.put("Time taken to execute the query 'Get station with highest average value of pollution' is: " + duration + " milliseconds and date is: "+date, Double.valueOf(stationCode));
+    }
+
+    public static void getTimeAndStationWithLowestAverageValueOfPollution(Table table, HashMap<String, Double> map) throws IOException {
+        long startTime = System.currentTimeMillis();
+        Scan scan = new Scan();
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("Station code"));
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("SO2"));
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("NO2"));
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("CO"));
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("O3"));
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("PM10"));
+        scan.addColumn(Bytes.toBytes("data"), Bytes.toBytes("PM2.5"));
+
+        ResultScanner resultScanner = table.getScanner(scan);
+
+        int stationCode = 0;
+        String date = "";
+        float minAvg = Float.MAX_VALUE;
+
+        for (Result result : resultScanner) {
+            String currentDate = Bytes.toString(result.getRow());
+            int currentStationCode = Integer.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("Station code"))));
+            float so2 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("SO2"))));
+            float no2 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("NO2"))));
+            float co = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("CO"))));
+            float o3 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("O3"))));
+            float pm10 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("PM10"))));
+            float pm25 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("PM2.5"))));
+
+            float avg = (so2 + no2 + co + o3 + pm10 + pm25) / 6;
+            if (avg < minAvg) {
+                minAvg = avg;
+                stationCode = currentStationCode;
+                date = currentDate;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        map.put("Time taken to execute the query 'Get station with lowest average value of pollution' is: " + duration + " milliseconds and date is: "+date, Double.valueOf(stationCode));
+    }
+
+    public static void getMonthWithHighestAveragePollution(Table table, HashMap<String,Double> map) throws IOException, ParseException {
+        long startTime = System.currentTimeMillis();
+        HashMap<String, Float> avgPollutionPerMonth = new HashMap<>();
+        Scan scan = new Scan();
+        scan.addFamily(Bytes.toBytes("data"));
+        ResultScanner scanner = table.getScanner(scan);
+        for (Result result : scanner) {
+            String date = Bytes.toString(result.getRow());
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            Date measurementDate = dateFormat.parse(date);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(measurementDate);
+            String month = new SimpleDateFormat("MMMM").format(calendar.getTime());
+            float so2 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("SO2"))));
+            float no2 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("NO2"))));
+            float co = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("CO"))));
+            float o3 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("O3"))));
+            float pm10 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("PM10"))));
+            float pm25 = Float.valueOf(Bytes.toString(result.getValue(Bytes.toBytes("data"), Bytes.toBytes("PM2.5"))));
+            float avgPollution = (so2 + no2 + co + o3 + pm10 + pm25) / 6;
+            if (!avgPollutionPerMonth.containsKey(month)) {
+                avgPollutionPerMonth.put(month, avgPollution);
+            } else {
+                float currentAvgPollution = avgPollutionPerMonth.get(month);
+                avgPollutionPerMonth.put(month, currentAvgPollution + avgPollution);
+            }
+        }
+        scanner.close();
+        double maxAvgPollution = Double.MIN_VALUE;
+        String monthWithHighestAvgPollution = "";
+        for (HashMap.Entry<String, Float> entry : avgPollutionPerMonth.entrySet()) {
+            double avgPollution = entry.getValue() / 645000;
+            if (avgPollution > maxAvgPollution) {
+                maxAvgPollution = avgPollution;
+                monthWithHighestAvgPollution = entry.getKey();
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        map.put("Time taken to execute the query 'Get month with highest average pollution' is: " + duration + " milliseconds is month:"+monthWithHighestAvgPollution, Double.valueOf(maxAvgPollution));
+    }
+
+
+    public static void main(String[] args) throws IOException, ParseException {
         Configuration conf = HBaseConfiguration.create();
         Connection connection = ConnectionFactory.createConnection(conf);
         Table table = connection.getTable(TableName.valueOf("air_pollution"));
@@ -249,6 +400,14 @@ public class Queries {
         getAllRowsWithValuesInASpecifiedRangeInMultipleColumnsAndExcludeRowsWithSpecificValueInAnotherColumn(table, "SO2", "0", "0.1", "PM10", "0", "100", "Station code", "125", map);
 
         getAllRowsWithValuesInASpecifiedRangeInASingleColumnAndReturnOnlySpecificColumns(table, "SO2", "0", "0.1", "date", "Station code", map);
+
+        getPM_MeasurementsInDecember(table, map);
+
+        getTimeAndStationWithHighestAverageValueOfPollution(table, map);
+
+        getTimeAndStationWithLowestAverageValueOfPollution(table, map);
+
+        getMonthWithHighestAveragePollution(table,map);
 
         Set<String> keys = map.keySet();
         for(String key: keys){
